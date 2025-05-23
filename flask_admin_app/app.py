a59bf92af9f181acb6ab185a88b221fb3d3e11e5
Le Van Hai <lenvanhai123@gmail.com>
2025-05-23 23:16:10 +0700

    Initial commit: Flask App with CI/CD

---
+++ flask_admin_app/app.py
@@ -0,0 +1,42 @@
+from flask import Flask
+from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
+from models import db, User, Product
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'supersecretkey'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)
+
+# Khởi tạo giao diện Flask-Admin
+admin = Admin(app, name='Quản trị hệ thống', template_mode='bootstrap4')
+
+# Tùy chỉnh giao diện User
+class UserAdmin(ModelView):
+    column_exclude_list = ['is_admin']
+    form_excluded_columns = ['is_admin']
+    can_create = True
+    can_edit = True
+    can_delete = True
+
+# Tùy chỉnh Product (tùy chọn thêm)
+class ProductAdmin(ModelView):
+    column_list = ['id', 'name', 'price', 'stock']
+    column_searchable_list = ['name']
+    column_filters = ['price', 'stock']
+    can_create = True
+    can_edit = True
+    can_delete = True
+
+# Thêm các bảng vào Admin
+admin.add_view(UserAdmin(User, db.session))
+admin.add_view(ProductAdmin(Product, db.session))
+
+# Tạo DB nếu chưa có
+with app.app_context():
+    db.create_all()
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ flask_admin_app/models.py
@@ -0,0 +1,21 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50))
+    email = db.Column(db.String(100))
+    is_admin = db.Column(db.Boolean, default=False)
+
+    def __repr__(self):
+        return f'<User {self.username}>'
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    price = db.Column(db.Float)
+    stock = db.Column(db.Integer)
+
+    def __repr__(self):
+        return f'<Product {self.name}>'
